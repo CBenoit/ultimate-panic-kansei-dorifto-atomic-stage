9fbdf1b2b00c79bf02593e0fa8b90250880faf1a
Benoît CORTIER <benoit.cortier@fried-world.eu>
2018-04-07 19:01:56 +0200

    Update car's script.

--- actors/kuruma/kuruma_body.gd
+++ actors/kuruma/kuruma_body.gd
@@ -1,57 +1,67 @@
 extends KinematicBody2D
 
+# constants
 const SPEED_GOAL = 25.0
-const MAX_SPEED = 35.0
+const MAX_SPEED = 45.0
 const MIN_SPEED = 10.0
 
 const ROTATION_SPEED = 0.75
 const MIN_ANGLE = -2 * PI / 3
 const MAX_ANGLE = -1 * PI / 3
 
-var speed = 10.0
-var drift = false
-var kansei_dorifto = false
+var normal_state_func = funcref(self, "_normal_state")
+var drift_state_func = funcref(self, "_drift_state")
+var kansei_drift_state_func = funcref(self, "_kansei_drift_state")
 
-func _ready():
-	pass
+# properties
+var speed = 10.0
+var state = normal_state_func
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
-		if Input.is_action_pressed("drift"):
-			if drift == false:
-				drift = true
-				$anim.play("drift_left")
-		else:
-			rotate(-ROTATION_SPEED / speed)
-	elif Input.is_action_pressed("ui_right"):
-		if Input.is_action_pressed("drift"):
-			if drift == false:
-				drift = true
-				$anim.play("drift_right")
-		else:
-			rotate(ROTATION_SPEED / speed)
-
-	if Input.is_action_just_released("drift") and drift:
-		drift = false
-		rotate($sprite.rotation)
-		$anim.stop()
-		$sprite.rotation = 0
+	state.call_func()
 
 	set_rotation(clamp(get_rotation(), MIN_ANGLE, MAX_ANGLE))
-
-	if drift:
-		speed -= 0.1
-	elif speed < SPEED_GOAL:
-		speed += 0.1
-	elif speed > SPEED_GOAL:
-		speed -= 0.1
-		
-	if Input.is_action_pressed("ui_accept") and kansei_dorifto == false and drift == false:
-		kansei_dorifto = true
-		speed += MAX_SPEED - SPEED_GOAL
-		$anim.play("kansei_dorifto")
-	
 	speed = clamp(speed, MIN_SPEED, MAX_SPEED)
 
 	var movement = Vector2(cos(rotation), sin(rotation)) * speed
 	move_and_collide(movement)
+
+func _normal_state():
+	if Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("drift"):
+			state = drift_state_func
+			$anim.play("drift_left")
+		else:
+			rotate(-ROTATION_SPEED / speed)
+	elif Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("drift"):
+			state = drift_state_func
+			$anim.play("drift_right")
+		else:
+			rotate(ROTATION_SPEED / speed)
+
+	if Input.is_action_pressed("ui_accept"):
+		state = kansei_drift_state_func
+		speed += MAX_SPEED - SPEED_GOAL
+		$anim.play("kansei_dorifto")
+		$anim.connect("animation_finished", self, "_kansei_drift_finished")
+
+	if speed < SPEED_GOAL:
+		speed += 0.1
+	elif speed > SPEED_GOAL:
+		speed -= 0.1
+
+func _drift_state():
+	speed -= 0.1
+	if Input.is_action_just_released("drift"):
+		state = normal_state_func
+		rotate($sprite.rotation)
+		$anim.stop()
+		$sprite.rotation = 0
+
+func _kansei_drift_state():
+	pass
+
+func _kansei_drift_finished(dummy):
+	state = normal_state_func
+	$anim.disconnect("animation_finished", self, "_kansei_drift_finished")
