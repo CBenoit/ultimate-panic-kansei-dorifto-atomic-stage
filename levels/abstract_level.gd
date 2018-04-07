040eb1347a9aa1a3ae2b7210cc5641baf4fd2d41
Benoît CORTIER <benoit.cortier@fried-world.eu>
2018-04-07 22:19:40 +0200

    Add some abstraction to level management.

--- levels/abstract_level.gd
+++ levels/abstract_level.gd
@@ -5,10 +5,9 @@ const road_scn = preload("res://objects/road/road.tscn")
 export(int) var road_length = 100000
 
 func _ready():
-	var x = $road.position.x
-	var step = get_node("road/sprite").get_texture().get_height()
-	for i in range(1, road_length / step):
-		add_road_at(x, -i * step)
+	var step = $background/road/sprite.get_texture().get_height()
+	for i in range(0, road_length / step):
+		add_road_at(0, -i * step)
 
 func add_road_at(x, y):
 	var inst = road_scn.instance()
