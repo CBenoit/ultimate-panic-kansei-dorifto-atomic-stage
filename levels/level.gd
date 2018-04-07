2568d12338810a573265f7ea3780c860440602e1
Benoît CORTIER <benoit.cortier@fried-world.eu>
2018-04-07 19:02:12 +0200

    Add road's expand system.

--- levels/level.gd
+++ levels/level.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
+var road_scn = preload("res://objects/road/road.tscn")
+
+export(int) var road_length = 100000
+
 func _ready():
-	pass
+	var x = $road.position.x
+	var step = get_node("road/sprite").get_texture().get_height()
+	for i in range(1, road_length / step):
+		add_road_at(x, -i * step)
+
+func add_road_at(x, y):
+	var inst = road_scn.instance()
+	inst.rotation = -PI / 2
+	inst.set_position(Vector2(x, y))
+	add_child(inst)
